d85031237578e82030166827e0c8817cb2a2a6e8
Adarsh Balu <adarshlp98@gmail.com>
2021-04-01 23:02:40 +0530

    Upgraded dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.6] 2021-04-01
+
+- Upgraded dependencies.
+
 ## [0.0.5] 2020-05-10
 
 - Updated Readme.
--- lib/services/network.dart
+++ lib/services/network.dart
@@ -1,5 +1,6 @@
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 class NetworkHelper {
   NetworkHelper();
@@ -11,7 +12,8 @@ class NetworkHelper {
     } else {
       print('Sending Failed');
       var data = jsonDecode(response.body);
-      print('Error Code : ' + data['code'].toString());	//Spelling changed from 'Codde' to 'Code'
+      print('Error Code : ' +
+          data['code'].toString()); //Spelling changed from 'Codde' to 'Code'
       print('Error Message : ' + data['message']);
       print("More info : " + data['more_info']);
     }
@@ -19,7 +21,7 @@ class NetworkHelper {
   }
 
   Future getRequest(String url) async {
-    http.Response response = await http.get(url);
+    http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       var data = response.body;
       return jsonDecode(data);
