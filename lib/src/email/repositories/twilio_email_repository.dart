2f0ebc5683031420a1966f07a24fc0dbf75035be
Adarsh Balachandran <adarshlp98@gmail.com>
2024-02-25 01:06:24 +0530

    Update project to start email feature

---
+++ lib/src/email/repositories/twilio_email_repository.dart
@@ -0,0 +1,11 @@
+abstract class TwilioEmailRepository {
+  Future sendEmail();
+}
+
+class TwilioEmailRepositoryImpl extends TwilioEmailRepository {
+  @override
+  Future sendEmail() {
+    // TODO: implement sendEmail
+    throw UnimplementedError();
+  }
+}
