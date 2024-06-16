d66f0dc203789fc4557b92cbbe3657267f958673
Adarsh Balachandran <adarshlp98@gmail.com>
2024-06-16 13:33:55 +0530

    Minor renaming

--- lib/twilio_flutter.dart
+++ lib/twilio_flutter.dart
@@ -1,7 +1,7 @@
 library twilio_flutter;
 
-export 'src/core/twilio_flutter_service.dart';
-export 'src/core/twilio_messaging_service.dart';
+export 'src/core/twilio_flutter_core.dart';
+export 'src/core/twilio_messaging_core.dart';
 export 'src/shared/dto/error_data.dart';
 export 'src/shared/dto/twilio_response.dart';
 export 'src/shared/exceptions/twilio_flutter_exception.dart';
